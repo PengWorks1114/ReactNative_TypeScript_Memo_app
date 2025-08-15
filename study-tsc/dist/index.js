a58b3f84e91f80673527c18c9ff066a69e6f6d2d
peng jacob <pengjacob@pengdeMac-mini.local>
2025-08-16 06:34:23 +0900

    init test file

---
+++ study-tsc/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+const message = "Hello World!";
+// const message: string = 100;
+console.log(message);
+const userName = "John";
+console.log(hello(userName));
+function hello(name) {
+    return `こんにちは、${name}さん`;
+}
